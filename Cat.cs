c62be70e6400ca91dbbf1bd3e3c20e381a60db53
bhandziak <bartek.handziak@gmail.com>
2025-04-07 00:04:10 +0200

    1 commit

---
+++ Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Project
+{
+    public class Cat : Animal
+    {
+        public string Breed { get; set; }
+
+        public Cat(string name, int age, Owner owner, string breed) : base(name, age, owner)
+        {
+            Breed = breed;
+        }
+
+        public override Cat ShallowClone()
+        {
+            return (Cat)this.MemberwiseClone();
+        }
+
+        public override Cat DeepClone()
+        {
+            Cat clone = (Cat)this.MemberwiseClone();
+            clone.Owner = new Owner(Owner.Name, Owner.Surname);
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", age: " + Age + ", owner: " + Owner.Name + " " + Owner.Surname
+                + ", breed: " + Breed;
+        }
+    }
+}
